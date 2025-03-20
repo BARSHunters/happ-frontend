5b02bb0fa61ccec396448c14eb9409b6d0cf4cc7
Sterus4 <368581@niuitmo.ru>
2025-03-20 19:14:50 +0300

    Messages Receives but not on background

---
+++ app/src/main/java/com/example/happ_frontend/model/notifications/FireBaseNotificationService.kt
@@ -0,0 +1,36 @@
+package com.example.happ_frontend.model.notifications
+
+import android.util.Log
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class FireBaseNotificationService : FirebaseMessagingService() {
+    override fun onCreate() {
+        super.onCreate()
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful){
+                Log.d("FCM", "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+            val token = task.result
+            Log.d("FCM", "Saved token: $token")
+        })
+    }
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+        Log.d("FCM", "New token: $token")
+        //TODO отправить этот токен на сервер
+
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+        println("Receive one message")
+        Log.d("Receive", "From: ${message.from}")
+        message.notification?.let {
+            Log.d("New message:", "Message Notification Body: ${it.body}")
+        }
+    }
+}
