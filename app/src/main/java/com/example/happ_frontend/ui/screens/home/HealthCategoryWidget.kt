3766526fbe84e19fafae4d24de89b5215071a563
Vadim Chaykin <snusmumrike2@gmail.com>
2025-03-23 00:50:51 +0300

    fixed HealthCategoryWidget spacer

--- app/src/main/java/com/example/happ_frontend/ui/screens/home/HealthCategoryWidget.kt
+++ app/src/main/java/com/example/happ_frontend/ui/screens/home/HealthCategoryWidget.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -96,6 +97,8 @@ fun HealthCategoryWidget(
                         style = MaterialTheme.typography.headlineMedium
                     )
                 }
+            } else {
+                Spacer(Modifier)
             }
             Icon(
                 imageVector = iconVector,
