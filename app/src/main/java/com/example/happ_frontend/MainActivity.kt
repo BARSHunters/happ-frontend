ccad2ebebd5aa055c011fa9a9dc618b30192279a
KruglovEgor <367327@edu.itmo.ru>
2025-03-29 01:35:01 +0300

    init

--- app/src/main/java/com/example/happ_frontend/MainActivity.kt
+++ app/src/main/java/com/example/happ_frontend/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.example.happ_frontend
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.happ_frontend.ui.domain.ActivityActivity
 import com.example.happ_frontend.ui.theme.HappfrontendTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +24,24 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HappfrontendTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        contentAlignment = Alignment.Center // Центрирование содержимого
+                    ) {
+                        Button(
+                            onClick = {
+                                val intent = Intent(this@MainActivity, ActivityActivity::class.java)
+                                startActivity(intent)
+                            }
+                        ) {
+                            Text("Nutrition")
+                        }
+                    }
                 }
             }
         }
