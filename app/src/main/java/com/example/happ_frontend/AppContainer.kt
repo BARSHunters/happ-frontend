d8c3ed7576b90770738428ea36cb649385ef3cd7
Sterus4 <368581@niuitmo.ru>
2025-03-29 13:21:51 +0300

    Изменил Базу данных на общую

--- app/src/main/java/com/example/happ_frontend/AppContainer.kt
+++ app/src/main/java/com/example/happ_frontend/AppContainer.kt
@@ -2,7 +2,6 @@ package com.example.happ_frontend
 
 import android.content.Context
 import com.example.happ_frontend.model.notifications.data.MyNotificationsRepository
-import com.example.happ_frontend.model.notifications.data.NotificationsDatabase
 import com.example.happ_frontend.model.notifications.data.NotificationsRepository
 
 interface AppContainer {
@@ -11,6 +10,6 @@ interface AppContainer {
 
 class AppDataContainer(private val context: Context) : AppContainer{
     override val notificationsRepository: NotificationsRepository by lazy {
-        MyNotificationsRepository(NotificationsDatabase.getDatabase(context).notificationDAO())
+        MyNotificationsRepository(HappFrontendDatabase.getDatabase(context).notificationDAO())
     }
 }
--- app/src/main/java/com/example/happ_frontend/HappFrontendDatabase.kt
+++ app/src/main/java/com/example/happ_frontend/HappFrontendDatabase.kt
@@ -1,27 +1,29 @@
-package com.example.happ_frontend.model.notifications.data
+package com.example.happ_frontend
 
 import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.happ_frontend.model.notifications.data.NotificationDAO
+import com.example.happ_frontend.model.notifications.data.NotificationEntity
 
 @Database(
     entities = [NotificationEntity::class],
     version = 1,
     exportSchema = false
 )
-abstract class NotificationsDatabase : RoomDatabase(){
+abstract class HappFrontendDatabase : RoomDatabase(){
     abstract fun notificationDAO() : NotificationDAO
     companion object {
         @Volatile
-        private var Instance : NotificationsDatabase? = null
+        private var Instance : HappFrontendDatabase? = null
 
-        fun getDatabase(context: Context) : NotificationsDatabase {
+        fun getDatabase(context: Context) : HappFrontendDatabase {
             return Instance ?: synchronized(this){
                 Room.databaseBuilder(
                     context,
-                    NotificationsDatabase::class.java,
-                    "notifications_database"
+                    HappFrontendDatabase::class.java,
+                    "happ_frontend_database"
                 )
                     .fallbackToDestructiveMigration()
                     .build().also { Instance = it }
