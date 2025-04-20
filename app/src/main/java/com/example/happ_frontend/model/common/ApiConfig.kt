9a0b1496a6f512437f6e12e316ec8d18087a844d
BIZHAN28 <bizhanov.rasul@mail.ru>
2025-04-20 21:16:54 +0300

    fix: revert config change

--- app/src/main/java/com/example/happ_frontend/model/common/ApiConfig.kt
+++ app/src/main/java/com/example/happ_frontend/model/common/ApiConfig.kt
@@ -1,6 +1,6 @@
 package com.example.happ_frontend.model.common
 
 object ApiConfig {
-    const val DEFAULT_BASE_URL = "http://127.0.0.1:3001/"
+    const val DEFAULT_BASE_URL = "http://10.0.2.2:8080/"
     const val DEFAULT_API_TIMEOUT_SECONDS = 5L
 }
