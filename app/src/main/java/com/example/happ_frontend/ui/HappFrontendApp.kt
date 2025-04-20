a5ddb32d0019c12cb8a463813578fe25a80d5a93
KruglovEgor <367327@edu.itmo.ru>
2025-04-20 22:14:07 +0300

    rebase fix
    Не знаю почему, но тут оказался очень старый код

--- app/src/main/java/com/example/happ_frontend/ui/HappFrontendApp.kt
+++ app/src/main/java/com/example/happ_frontend/ui/HappFrontendApp.kt
@@ -1,28 +1,11 @@
 package com.example.happ_frontend.ui
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.navigation.compose.rememberNavController
-import com.example.happ_frontend.ui.navigation.ActivityDest
 import com.example.happ_frontend.ui.navigation.HappFrontendNavigationHost
-import com.example.happ_frontend.ui.navigation.NutritionDest
-import com.example.happ_frontend.ui.screens.nutrition.NutritionScreen
 
 @Composable
-fun HappFrontendApp(){
+fun HappFrontendApp() {
     val navigationController = rememberNavController()
-
-    //TODO fix
-    LaunchedEffect(navigationController) {
-        navigationController.navigate(ActivityDest.route) {
-            popUpTo(navigationController.graph.startDestinationId) {
-                inclusive = true
-            }
-        }
-    }
-
     HappFrontendNavigationHost(navigationController = navigationController)
-
-
-
 }
