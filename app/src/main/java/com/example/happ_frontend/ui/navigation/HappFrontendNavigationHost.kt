493d6848cdc994820120e9bf7425ad5849755673
KruglovEgor <367327@edu.itmo.ru>
2025-04-20 22:05:13 +0300

    Update HappFrontendNavigationHost.kt

--- app/src/main/java/com/example/happ_frontend/ui/navigation/HappFrontendNavigationHost.kt
+++ app/src/main/java/com/example/happ_frontend/ui/navigation/HappFrontendNavigationHost.kt
@@ -1,5 +1,6 @@
 package com.example.happ_frontend.ui.navigation
 
+import android.util.Log
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
